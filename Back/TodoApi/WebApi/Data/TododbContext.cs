c9eb28b733ab46c312906768ab8f917030a450cb
Kostan <6PwCnSEPh2NHrEA>
2024-02-13 21:22:39 +0300

    add dbcontext and some models for him

---
+++ Back/TodoApi/WebApi/Data/TododbContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data;
+
+public partial class TododbContext : DbContext
+{
+    public TododbContext()
+    {
+    }
+
+    public TododbContext(DbContextOptions<TododbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<RolesInfo> RolesInfos { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Username=postgres;Password=postgres;Database=tododb");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("roles");
+
+            entity.Property(e => e.RoleId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("role_id");
+            entity.Property(e => e.UserId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("user_id");
+
+            entity.HasOne(d => d.RoleNavigation).WithMany()
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("fk_role");
+
+            entity.HasOne(d => d.User).WithMany()
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("fk_user");
+        });
+
+        modelBuilder.Entity<RolesInfo>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("roles_pk");
+
+            entity.ToTable("roles_info");
+
+            entity.Property(e => e.RoleId).HasColumnName("role_id");
+            entity.Property(e => e.Title)
+                .HasColumnType("character varying")
+                .HasColumnName("title");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("users_pk");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.Email)
+                .HasColumnType("character varying")
+                .HasColumnName("email");
+            entity.Property(e => e.HashPass)
+                .HasColumnType("character varying")
+                .HasColumnName("hash_pass");
+            entity.Property(e => e.Name)
+                .HasColumnType("character varying")
+                .HasColumnName("name");
+            entity.Property(e => e.Phone)
+                .HasColumnType("character varying")
+                .HasColumnName("phone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Back/TodoApi/WebApi/Migrations/20240213181712_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles_info",
+                columns: table => new
+                {
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("roles_pk", x => x.role_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying", nullable: false),
+                    email = table.Column<string>(type: "character varying", nullable: false),
+                    hash_pass = table.Column<string>(type: "character varying", nullable: false),
+                    phone = table.Column<string>(type: "character varying", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pk", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    role_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "fk_role",
+                        column: x => x.role_id,
+                        principalTable: "roles_info",
+                        principalColumn: "role_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_user",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_role_id",
+                table: "roles",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_user_id",
+                table: "roles",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "roles_info");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
