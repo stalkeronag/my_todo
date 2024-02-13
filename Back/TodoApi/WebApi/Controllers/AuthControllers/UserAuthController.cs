8c3974ed9346df6054a928d3670bdbc05a5594a8
Kostan <6PwCnSEPh2NHrEA>
2024-02-13 15:57:22 +0300

    add few interfaces service

---
+++ Back/TodoApi/WebApi/Controllers/AuthControllers/UserAuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Dto;
+
+namespace WebApi.Controllers.AuthControllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class UserAuthController : Controller
+    {
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            return Ok(loginDto);
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            return Ok(registerDto);
+        }
+    }
+}
