f805098505a69a0aefbcfc6ab8e4e5041c60fc04
Kostan <6PwCnSEPh2NHrEA>
2024-02-20 18:08:53 +0300

    new commit

--- Back/TodoApi/WebApi/Services/Interfaces/IUserService.cs
+++ Back/TodoApi/WebApi/Services/Interfaces/IUserService.cs
@@ -3,5 +3,14 @@
     public interface IUserService
     {
         public void GetUser();
+
+        public void AddUser();
+
+        public void UpdateUser();
+
+        public void DeleteUser();
+
+        public void BunUser();
+
     }
 }
